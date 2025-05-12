b254ab33c23b4f9664c640b359770231267e5da4
ANGELUSD11 <angelleonardohern3@gmail.com>
2025-05-11 22:57:42 -0500

    mensaje

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 print("hello world")
 print("Push externo1")
 print("hello test")
@@ -6,16 +5,3 @@ print("hello test1")
 print("hello test2")
 print("hello test3")
 print("hello test4")
-=======
-
-print("hELLO TEST")
-
-print("hello world")
-
-print("Hello 2")
-print("Hello 3")
-print("Hello 4")
-print("Hello 5")
-print("hello 6")
-
->>>>>>> ebf6a0eda611cb38f64ec3e51369967b2873f1d7
